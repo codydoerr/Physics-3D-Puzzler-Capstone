cbc368e31f578a585aec3261b8ea3e6c351adff3
codydoerr <40671276+codydoerr@users.noreply.github.com>
2023-02-13 05:37:43 -0600

    working on opening doors

---
+++ Assets/EnvironmentController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnvironmentController : MonoBehaviour
+{
+    KeyCode use = KeyCode.E;
+    HingeJoint[] movingAsset = new HingeJoint[2];
+    [SerializeField] GameObject staticAsset;
+    bool isUsable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isUsable = false;
+        movingAsset = staticAsset.GetComponents<HingeJoint>();
+    }
+    public void MakeUsable()
+    {
+        isUsable = true;
+    }
+    public void MakeUnUsable()
+    {
+
+        isUsable = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(use) && isUsable)
+        {
+            var motor = movingAsset[0].motor;
+            motor.targetVelocity *= -1;
+            foreach (HingeJoint aMotor in movingAsset)
+            {
+                aMotor.motor = motor;
+            }
+        }
+    }
+}
--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,7 +8,7 @@ public class MouseLook : MonoBehaviour
     [SerializeField] Transform playerBody;
     [SerializeField] Transform camera;
     float xRotation = 0f;
-
+    GameObject lastEnvironmentEnabled;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +29,19 @@ public class MouseLook : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+    private void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(camera.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, fwd, out hit, 5f) && hit.collider.gameObject.GetComponent<EnvironmentController>()!=null)
+        {
+            lastEnvironmentEnabled = hit.collider.gameObject;
+            hit.collider.gameObject.GetComponent<EnvironmentController>().MakeUsable();
+        }
+        else if(hit.collider.gameObject.GetComponent<EnvironmentController>() != null)
+        {
+            hit.collider.gameObject.GetComponent<EnvironmentController>().MakeUnUsable();
+        }
     }
 }
