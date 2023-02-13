bd8489e76728dbf1624c4341be55625e0a18786c
kaolald <kaolald@ilstu.edu>
2023-02-13 17:01:28 -0600

    added basic music script

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -7,18 +7,35 @@ public class MusicController : MonoBehaviour
     [SerializeField] AudioClip mus1;
     [SerializeField] AudioClip mus2;
     [SerializeField] AudioClip mus3;
-    AudioSource audioSource;
+    public GameObject musicElement;
+    public AudioSource audioSource;
+    private int timesHit;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.clip = mus1; 
-        audioSource.Play();
+        audioSource = GetComponent<AudioSource>();
+        timesHit = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if (timesHit > 3) 
+       {
+        timesHit = 0;
+       } 
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "pellet") 
+        {
+            audioSource.clip = mus1;
+            audioSource.Play();
+
+            timesHit++;
+            Debug.Log("trigger hit!");
+        }
     }
 }
