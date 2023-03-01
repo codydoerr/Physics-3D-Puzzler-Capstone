42a67f2023d2c16dae11c4e9ba2d3f4b74bab870
kaolald <kaolald@ilstu.edu>
2023-03-01 15:24:17 -0600

    revised music controller script

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -7,24 +7,29 @@ public class MusicController : MonoBehaviour
     [SerializeField] AudioClip mus1;
     [SerializeField] AudioClip mus2;
     [SerializeField] AudioClip mus3;
+    [SerializeField] AudioClip mus4;
+
     public GameObject musicElement;
     public AudioSource audioSource;
-    private int timesHit;
-
+    private float stemVol;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        timesHit = 0;
+        stemVol = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (timesHit > 3) 
+       if (stemVol < 0.0f)
        {
-        timesHit = 0;
-       } 
+            stemVol = 0.0f;
+       }
+       else if (stemVol > 1.0f)
+       {
+            stemVol = 1.0f;
+       }
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -33,8 +38,8 @@ public class MusicController : MonoBehaviour
         {
             audioSource.clip = mus1;
             audioSource.Play();
-
-            timesHit++;
+            audioSource.volume = stemVol;
+            //stemVol + 0.1f;
             Debug.Log("trigger hit!");
         }
 
@@ -42,6 +47,8 @@ public class MusicController : MonoBehaviour
         {
             audioSource.clip = mus2;
             audioSource.Play();
+            audioSource.volume = stemVol;
+            //stemVol + 0.1f;
 
             Debug.Log("player trigger");
         }
