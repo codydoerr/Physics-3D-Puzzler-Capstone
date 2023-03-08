08329f66c140ecae5a80ae74f7b4f29738678434
kaolald <kaolald@ilstu.edu>
2023-03-07 19:08:48 -0600

    added seperate stem and music scripts

--- Assets/Scripts/StemController.cs
+++ Assets/Scripts/StemController.cs
@@ -2,40 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicController : MonoBehaviour
+public class StemController : MonoBehaviour
 {
-    [SerializeField] AudioClip mus1;
-    [SerializeField] AudioClip mus2;
-    [SerializeField] AudioClip mus3;
-    [SerializeField] AudioClip mus4;
     public GameObject musicElement;
     public AudioSource audioSource;
+    public float stemVol;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        //AudioClip = audioSource.clip;
+        audioSource.Play();
+        stemVol = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        audioSource.volume = stemVol;
     }
 
-    public void OnTriggerEnter(Collider collision)
+    public void OnTriggerStay(Collider collision)
     {
         if (collision.tag == "pellet") 
         {
-            audioSource.clip = mus1;
-            audioSource.Play();
+            stemVol = Mathf.Lerp(stemVol, 1, 0.25f * Time.deltaTime);
+            Debug.Log(stemVol);
             Debug.Log("trigger hit!");
         }
 
         if (collision.tag == "Player") 
         {
-            audioSource.clip = mus2;
-            audioSource.Play();
+            stemVol = Mathf.Lerp(stemVol, 1, 0.5f * Time.deltaTime);
+            Debug.Log(stemVol);
             Debug.Log("player trigger");
         }
     }
