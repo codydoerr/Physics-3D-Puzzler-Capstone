f7440584765818b51f08f89dd38a19e761aa4d65
dadams091 <dwadam2@ilstu.edu>
2023-04-05 00:13:06 -0500

    scripting key work

--- Assets/Scripts/InteractController.cs
+++ Assets/Scripts/InteractController.cs
@@ -10,6 +10,7 @@ public class InteractController : MonoBehaviour
 {
     public GameObject KeyToDoor;
     public bool hasKey;
+    public bool inBox;
     public Text interactText;
     float raycastDistance = 3;
     // Start is called before the first frame update
@@ -21,13 +22,18 @@ public class InteractController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // This creates a 'ray' at the Main Camera's Centre Point essentially the centre of the users Screen
-        RaycastHit hit; //This creates a Hit which is used to callback the object that was hit by the Raycast
-        if (Input.GetKeyDown(KeyCode.E))
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // This creates a 'ray' at the Main Camera's Centre Point essentially the centre of the users Screen
+        //RaycastHit hit; //This creates a Hit which is used to callback the object that was hit by the Raycast
+        if (Input.GetKeyDown(KeyCode.E) && inBox)
         {
-            print("E was pressed");
+            print("Player has Key");
             KeyToDoor.SetActive(false);
             hasKey = true;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inBox = true;
     }
 }
