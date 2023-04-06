45cf3e224bc0ddb2225761fdd05d4d3c58955501
dadams091 <dwadam2@ilstu.edu>
2023-04-05 23:22:04 -0500

    added ui to key interaction

--- Assets/Scripts/InteractController.cs
+++ Assets/Scripts/InteractController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 //https://answers.unity.com/questions/1917193/is-there-a-way-to-make-a-press-e-to-interact-when.html
 
@@ -10,30 +11,37 @@ public class InteractController : MonoBehaviour
 {
     public GameObject KeyToDoor;
     public bool hasKey;
-    public bool inBox;
-    public Text interactText;
-    float raycastDistance = 3;
+    public bool insideBox;
+    public TextMeshProUGUI interactText;
+
     // Start is called before the first frame update
     void Start()
     {
         hasKey = false;
+        insideBox = false;
+        interactText.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // This creates a 'ray' at the Main Camera's Centre Point essentially the centre of the users Screen
-        //RaycastHit hit; //This creates a Hit which is used to callback the object that was hit by the Raycast
-        if (Input.GetKeyDown(KeyCode.E) && inBox)
+        if (Input.GetKeyDown(KeyCode.E) && insideBox)
         {
             print("Player has Key");
             KeyToDoor.SetActive(false);
             hasKey = true;
+            interactText.enabled = false;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        inBox = true;
+        insideBox = true;
+        interactText.enabled = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        insideBox = false;
+        interactText.enabled = false;
     }
 }
