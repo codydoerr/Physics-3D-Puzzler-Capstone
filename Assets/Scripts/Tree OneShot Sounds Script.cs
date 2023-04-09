1b2c58d5d43d8e377d10f3eed2c9e7c15160a4b6
briric1 <89150953+briric1@users.noreply.github.com>
2023-04-09 15:27:19 -0500

    added in 3 tree sounds and script for them
    hopefully this code is sufficient cuz I don't 100% understand it myself but I'm sure Cody will

---
+++ Assets/Scripts/Tree OneShot Sounds Script.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OneShotController : MonoBehaviour
+{
+    [SerializeField] float destroyTime;
+    [SerializeField] AudioMixerGroup mixerGroup;
+    AudioSource aS;
+    int currentClipIndex = 0;
+    int[] clips = {1, 2, 3}; // integer values to represent the sounds
+
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+        aS.outputAudioMixerGroup = mixerGroup;
+        PlayNextClip();
+        if (destroyTime > 0) {
+            Destroy(gameObject, destroyTime);
+        } else {
+            Destroy(gameObject, aS.clip.length);
+        }
+    }
+
+    void PlayNextClip() {
+        if (currentClipIndex >= clips.Length) {
+            return;
+        }
+        int clipToPlay = clips[currentClipIndex];
+        currentClipIndex++;
+
+       
+        if (clipToPlay == 1) {
+            aS.clip = Resources.Load<AudioClip>("Sound1");
+        } else if (clipToPlay == 2) {
+            aS.clip = Resources.Load<AudioClip>("Sound2");
+        } else if (clipToPlay == 3) {
+            aS.clip = Resources.Load<AudioClip>("Sound3");
+        }
+        aS.Play();
+    }
+
+    void OnDestroy()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayNextClip();
+    }
+}
