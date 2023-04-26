93a6db0a2fe1811a82f06254770da5f8f0a4641e
codydoerr <40671276+codydoerr@users.noreply.github.com>
2023-04-26 01:43:51 -0500

    stuff

--- Assets/Scripts/MaskObject.cs
+++ Assets/Scripts/MaskObject.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class MaskObject : MonoBehaviour
 {
-    public GameObject[] maskObj;
+    public GameObject maskObj;
     // Start is called before the first frame update
     void Start()
-    {
-        foreach(GameObject maskObj in maskObj)
-        {
+    { 
             maskObj.GetComponent<MeshRenderer>().material.renderQueue = 3002;
-        }
     }
 
     // Update is called once per frame
