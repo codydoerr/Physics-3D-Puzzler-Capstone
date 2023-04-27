a16b92bac86366d622992e7d7783eaa3ab251a6c
codydoerr <40671276+codydoerr@users.noreply.github.com>
2023-04-27 06:09:24 -0500

    papers

--- Assets/HUDController.cs
+++ Assets/HUDController.cs
@@ -10,16 +10,33 @@ public class HUDController : MonoBehaviour
     [SerializeField] Sprite basicCross;
 
     [SerializeField] Sprite cameraCross;
+    [SerializeField] GameObject[] notes;
 
     public void SwapCrosshair()
     {
         if (crosshair.GetComponent<Image>().sprite == basicCross) { 
-
             crosshair.GetComponent<Image>().sprite = cameraCross;
         }
         else
         {
             crosshair.GetComponent<Image>().sprite = basicCross;
+        }
+    }
+    public void ActivateNote(int noteNum)
+    {
+
+        Debug.Log("note num" + noteNum);
+        Debug.Log("the note" + notes[noteNum - 1]);
+        notes[noteNum-1].SetActive(true);
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (GameObject item in notes)
+            {
+                item.SetActive(false);
+            }
         }
     }
 }
